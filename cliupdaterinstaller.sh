c4caf25d4eeb8bb98fe1818d42cf2a44095bad07
AZMindroma / Furkan <69211110+AZMindroma@users.noreply.github.com>
2024-01-14 19:05:41 +0100

    Add files via upload

---
+++ cliupdaterinstaller.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+folder_path="$HOME/Programs"
+file_path="$HOME/Programs/nobara-update"
+line='export PATH=$PATH:$HOME/Programs'
+
+if [ -d "$folder_path" ]; then
+	echo "The Programs folder already exists."
+else
+	echo "Creating Programs folder..."
+	mkdir $folder_path
+	echo "Created."
+fi
+
+if [ -e "$file_path" ]; then
+	echo "The nobara-update file already exists."
+else
+	echo "Adding nobara-update file..."
+	echo "sudo dnf update rpmfusion-nonfree-release rpmfusion-free-release fedora-repos nobara-repos --refresh && sudo dnf distro-sync --refresh && sudo dnf update --refresh" >> $file_path
+	chmod +x $file_path
+	echo "Added."
+fi
+
+echo "Adding folder to PATH..."
+if grep -q "$line" "$HOME/.bashrc"; then
+	echo "Folder is already in PATH."
+else
+	echo $line >> $HOME/.bashrc
+	echo "Added."
+fi
+echo "Successfully installed. Restart your shell, subsequently run nobara-update to update your system."
+
